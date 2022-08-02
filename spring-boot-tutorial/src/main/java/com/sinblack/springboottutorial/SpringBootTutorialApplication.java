fa005e8349230f554e9fec3c8b4427e8ead4b53b
Luu Truong Sinh <sinhlt2@vingroup.net>
2022-08-02 17:20:27 +0700

    update chapter 4

--- spring-boot-tutorial/src/main/java/com/sinblack/springboottutorial/SpringBootTutorialApplication.java
+++ spring-boot-tutorial/src/main/java/com/sinblack/springboottutorial/SpringBootTutorialApplication.java
@@ -1,13 +1,29 @@
 package com.sinblack.springboottutorial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootTutorialApplication {
+
+  private static Logger log = LoggerFactory.getLogger(SpringBootTutorialApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootTutorialApplication.class, args);
 	}
 
+  @Value("${server.ip}")
+  String serverIp;
+
+  @Bean
+  CommandLineRunner values() {
+    return args -> {
+      log.info(" > Ther Server IP is: " + serverIp);
+    };
+  }
 }
--- spring-boot-tutorial/src/main/java/com/sinblack/springboottutorial/WebController.java
+++ spring-boot-tutorial/src/main/java/com/sinblack/springboottutorial/WebController.java
@@ -1,3 +1,11 @@
+package com.sinblack.springboottutorial;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+@RestController
 public class WebController {
-  
+  @GetMapping
+  public String index(){
+    return "Hello Spring Boot";
+  }
 }
