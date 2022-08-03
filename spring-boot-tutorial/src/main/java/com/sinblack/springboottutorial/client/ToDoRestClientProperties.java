cef189a2de77aa3c85c1222ec0eee61a546c3b60
Luu Truong Sinh <sinhlt2@vingroup.net>
2022-08-03 16:40:27 +0700

    update chapter04

---
+++ spring-boot-tutorial/src/main/java/com/sinblack/springboottutorial/client/ToDoRestClientProperties.java
@@ -0,0 +1,16 @@
+package com.sinblack.springboottutorial.client;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@ConfigurationProperties(prefix="todo")
+@Data
+public class ToDoRestClientProperties {
+  
+  private String url;
+  private String basePath;
+
+}
